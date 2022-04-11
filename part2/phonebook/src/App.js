57f389a43e12425455bbe6709720f11c5049196e
AJ <ameerul.junaidi@gmail.com>
2022-04-10 23:49:14 -0400

    Completed part 2 a extras

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Persons = ({persons}) => <>{persons.map(person => <p key={person.id}>{person.name} {person.number}</p>)}</>
+
+
+function Filter(props) {
+    return <form>
+        <div>
+            filter shown with
+            <input onChange={props.onChange}/>
+        </div>
+    </form>;
 }
 
-export default App;
+function PersonForm(props) {
+    return <form onSubmit={props.onSubmit}>
+        <div>
+            name:
+            <input value={props.name} onChange={props.onNameChange}/>
+        </div>
+        <div>
+            number:
+            <input value={props.number} onChange={props.onNumberChange}/>
+        </div>
+        <div>
+            <button type="submit">add</button>
+        </div>
+    </form>;
+}
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        {name: 'Arto Hellas', number: '040-123456', id: 1},
+        {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
+        {name: 'Dan Abramov', number: '12-43-234345', id: 3},
+        {name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
+    ])
+    const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState("")
+    const [filteredPersons, setFilteredPersons] = useState(persons)
+
+    const handleNameChange = (event) => setNewName(event.target.value)
+
+    const handleNumberChange = (event) => setNewNumber(event.target.value)
+
+    const addNumber = (event) => {
+        console.log('adding number')
+        event.preventDefault()
+        const newNameObject = {name: newName, number: newNumber, id: persons.length + 1}
+        const alreadyExist = persons.find(person => person.name === newName)
+
+        if (alreadyExist) {
+            alert(`${newName} is already added to phonebook`)
+        } else {
+            setPersons(persons.concat(newNameObject))
+        }
+
+        setFilteredPersons(persons)
+        setNewName("")
+        setNewNumber("")
+    }
+
+    const handleFilterChange = (event) => {
+        event.preventDefault()
+        if (event.target.value === "") {
+            setFilteredPersons(persons)
+        } else {
+            setFilteredPersons(persons.filter(person => {
+                const personName = person.name.toLowerCase()
+                const toCompare = event.target.value.toLowerCase()
+                return personName.includes(toCompare);
+            }))
+        }
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <Filter onChange={handleFilterChange}/>
+            <h2>add a new</h2>
+            <PersonForm
+                onSubmit={addNumber}
+                name={newName}
+                onNameChange={handleNameChange}
+                number={newNumber}
+                onNumberChange={handleNumberChange}
+            />
+            <h2>Numbers</h2>
+            <Persons persons={filteredPersons}/>
+        </div>
+    )
+}
+
+export default App

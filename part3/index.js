1eeddfc171079128db2239106176d85216d11670
AJ <ameerul.junaidi@gmail.com>
2022-04-18 00:19:47 -0400

    uibuild

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
 
 const Person = require('./models/person')
 
@@ -35,29 +35,20 @@ app.get('/api/phonebook', (_, response) => {
     })
 })
 
-app.get('/api/phonebook/:id', (request, response) => {
+app.get('/api/phonebook/:id', (request, response, next) => {
     const id = request.params.id
+
     Person.findById(id)
-        .then(person => {
-            response.json(person)
-        })
-        .catch(_ => {
-            response.status(404).end()
-        })
+        .then(person => response.json(person))
+        .catch(error => next(error))
 })
 
-app.delete('/api/phonebook/:id', (request, response) => {
+app.delete('/api/phonebook/:id', (request, response, next) => {
     const id = request.params.id
 
     Person.findByIdAndDelete(id)
-        .then(_ => {
-            response.status(204).end()
-            console.log('deleted person')
-        })
-        .catch(_ => {
-            response.status(404).end()
-            console.log('deleting failed')
-        })
+        .then(_ => response.status(204).end())
+        .catch(error => next(error))
 })
 
 app.post('/api/phonebook', (request, response) => {
@@ -79,22 +70,40 @@ app.post('/api/phonebook', (request, response) => {
     })
 })
 
-app.put('/api/phonebook/:id', (request, response) => {
+app.put('/api/phonebook/:id', (request, response, next) => {
     const id = request.params.id
     const body = request.body
 
-    Person.findByIdAndUpdate(id, body)
-        .then(_ => {
-            response.json(body)
-            console.log('updated person')
-        })
-        .catch(_ => {
-            response.status(404).end()
-            console.log('updating failed')
-        })
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(id, person, { new: true })
+        .then(_ => response.json(body))
+        .catch(error => next(error))
 })
 
+const unknownEndpoint = (_, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
 })

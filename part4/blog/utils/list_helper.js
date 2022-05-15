15a570d355af7ddbecd71d8223b9f7d771af9803
AJ <ameerul.junaidi@gmail.com>
2022-05-15 12:51:41 -0400

    Completed 4.4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,6 +1,9 @@
 // eslint-disable-next-line no-unused-vars
-const dummy = (blogs) => 1
+const dummy = (_) => 1
+
+const totalLikes = (blogs) => blogs.length === 0 ? 0 : blogs.reduce((total, blog) => total + blog.likes, 0)
 
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }

8cdeb3e601cfae34eab4b56f30e21526a9965bb0
AJ <ameerul.junaidi@gmail.com>
2022-05-15 13:11:52 -0400

    Completed 4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,9 +1,28 @@
 // eslint-disable-next-line no-unused-vars
 const dummy = (_) => 1
 
-const totalLikes = (blogs) => blogs.length === 0 ? 0 : blogs.reduce((total, blog) => total + blog.likes, 0)
+const totalLikes = (blogs) =>
+    blogs.length === 0
+        ? 0
+        : blogs.reduce((total, blog) => total + blog.likes, 0)
+
+const favoriteBlog = (blogs) => {
+    const maxLikes = blogs.length === 0
+        ? undefined
+        : blogs.reduce((max, current) => current.likes > max.likes ? current : max, {likes: 0})
+
+    if (!maxLikes) return undefined
+
+    return {
+        title: maxLikes.title,
+        author: maxLikes.author,
+        likes: maxLikes.likes
+    }
+}
+
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }

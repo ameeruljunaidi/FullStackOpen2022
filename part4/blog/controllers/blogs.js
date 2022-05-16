bfa9880295b59405a1cd29afd14e88ce90ab9470
AJ <ameerul.junaidi@gmail.com>
2022-05-15 22:22:51 -0400

    Added tests for blog app

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,18 +1,28 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    response.send('This is the blog app.')
+blogsRouter.get('/', async (request, response) => {
+    await response.send('This is the blog app.')
 })
 
-blogsRouter.get('/api/blogs', (request, response) => {
-    Blog.find({}).then(blogs => response.json(blogs))
+blogsRouter.get('/api/blogs', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
-blogsRouter.post('/api/blogs', (request, response) => {
+blogsRouter.post('/api/blogs', async (request, response) => {
     const blog = new Blog(request.body)
+    const result = await blog.save()
+    response.status(201).json(result)
+})
 
-    blog.save().then(result => response.status(201).json(result))
+blogsRouter.delete('/api/blogs/', async (request, response, next) => {
+    try {
+        await Blog.deleteMany({})
+        response.status(204).end()
+    } catch (e) {
+        next(e)
+    }
 })
 
 module.exports = blogsRouter
--- part4/blog/tests/most_blogs.test.js
+++ part4/blog/tests/most_blogs.test.js
@@ -1,68 +1,7 @@
 const mostBlogs = require('../utils/list_helper').mostBlogs
+const { listWithOneBlog, listWithManyBlogs } = require('./test_data')
 
 describe('Most active blogger', () => {
-    const listWithOneBlog = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 5,
-            __v: 0
-        }
-    ]
-
-    const manyBlogs = [
-        {
-            _id: '5a422a851b54a676234d17f7',
-            title: 'React patterns',
-            author: 'Michael Chan',
-            url: 'https://reactpatterns.com/',
-            likes: 7,
-            __v: 0
-        },
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 5,
-            __v: 0
-        },
-        {
-            _id: '5a422b3a1b54a676234d17f9',
-            title: 'Canonical string reduction',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-            likes: 12,
-            __v: 0
-        },
-        {
-            _id: '5a422b891b54a676234d17fa',
-            title: 'First class tests',
-            author: 'Robert C. Martin',
-            url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-            likes: 10,
-            __v: 0
-        },
-        {
-            _id: '5a422ba71b54a676234d17fb',
-            title: 'TDD harms architecture',
-            author: 'Robert C. Martin',
-            url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-            likes: 0,
-            __v: 0
-        },
-        {
-            _id: '5a422bc61b54a676234d17fc',
-            title: 'Type wars',
-            author: 'Robert C. Martin',
-            url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-            likes: 2,
-            __v: 0
-        }
-    ]
-
     test('of empty list is undefined', () => {
         const result = mostBlogs([])
         expect(result).toBe(undefined)
@@ -77,7 +16,7 @@ describe('Most active blogger', () => {
     })
 
     test('of a bigger list is right', () => {
-        const result = mostBlogs(manyBlogs)
+        const result = mostBlogs(listWithManyBlogs)
         expect(result).toEqual({
             author: 'Robert C. Martin',
             blogs: 3

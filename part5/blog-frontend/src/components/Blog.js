45c6c055dcc5f2925a1da318fbfc755a98c1e9b8
AJ <ameerul.junaidi@gmail.com>
2022-10-30 13:31:58 -0700

    Completed 5.7, added a detailed view for the blog

--- part5/blog-frontend/src/components/Blog.js
+++ part5/blog-frontend/src/components/Blog.js
@@ -1,6 +1,9 @@
 import blogService from '../services/blogs'
+import { useState } from 'react'
 
 const Blog = ({ blog, setBlogs, showNotification }) => {
+    const [showDetail, setShowDetail] = useState(false)
+
     const handleDeleteBlog = async (event, id) => {
         event.preventDefault()
 
@@ -14,15 +17,52 @@ const Blog = ({ blog, setBlogs, showNotification }) => {
         }
     }
 
-    return (
-        <>
+    const handleToggleDetail = () => setShowDetail(prevState => !prevState)
+
+    const handleAddLikes = () => console.log('to do handle add likes')
+
+    const blogStyle = {
+        paddingTop: 5,
+        paddingBottom: 5,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+
+    const basicView = () => (
+        <div>
             <div className="inline">
                 {blog.title} {blog.author}
-                <button onClick={event => handleDeleteBlog(event, blog.id)}>delete</button>
+                <button onClick={handleToggleDetail}>view</button>
             </div>
             <br></br>
-        </>
+        </div>
     )
+
+    const detailedView = () => (
+        <div style={blogStyle}>
+            <div className="inline">
+                {blog.title}
+                <button onClick={handleToggleDetail}>hide</button>
+            </div>
+            <div>
+                {blog.url}
+            </div>
+            <div className="inline">
+                {blog.likes} likes
+                <button onClick={handleAddLikes}>like</button>
+            </div>
+            <div>
+                {blog.author}
+            </div>
+            <div>
+                <button onClick={event => handleDeleteBlog(event, blog.id)}>delete blog</button>
+            </div>
+        </div>
+    )
+
+    return showDetail ? detailedView() : basicView()
 }
 
 export default Blog

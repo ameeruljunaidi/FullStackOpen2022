1769868c4eb281e10a60bac6a83e6a205b7309b4
AJ <ameerul.junaidi@gmail.com>
2022-11-12 17:31:39 -0800

    Completed 5.18, tested login functionality

--- part5/blog-frontend/cypress/e2e/blog_app.cy.js
+++ part5/blog-frontend/cypress/e2e/blog_app.cy.js
@@ -1,10 +1,11 @@
 describe('Blog app', () => {
     beforeEach(() => {
         cy.request('POST', 'http://localhost:3003/api/testing/reset')
+        cy.addUser({ name: 'test-name', username: 'test-username', password: 'test-password' })
         cy.visit('http://localhost:3000')
     })
 
-    it('login form is shown', () => {
+    it('Login form is shown', () => {
         cy.contains('blogs')
         cy.contains('username')
         cy.contains('password')
@@ -12,13 +13,26 @@ describe('Blog app', () => {
         cy.contains('log in to view blogs')
     })
 
-    // it('can log in the user', () => {
-    //     cy.get('#username-input').type('initial-username')
-    //     cy.get('#password-input').type('this-is-a-password')
-    //     cy.get('#login-button').click()
-    //     cy.contains('initial-name logged in')
-    // })
-    //
+    describe('Login', () => {
+        it('succeeds with the correct credentials', () => {
+            cy.get('#username-input').type('test-username')
+            cy.get('#password-input').type('test-password')
+            cy.get('#login-button').click()
+            cy.get('.success')
+                .should('contain', 'Successfully logged in')
+                .and('have.css', 'color', 'rgb(0, 128, 0)')
+        })
+
+        it('fails with the wrong password', () => {
+            cy.get('#username-input').type('test-username')
+            cy.get('#password-input').type('wrong-password')
+            cy.get('#login-button').click()
+            cy.get('.error')
+                .should('contain', 'Wrong credentials')
+                .and('have.css', 'color', 'rgb(255, 0, 0)')
+        })
+    })
+
     // describe('when logged in', () => {
     //     beforeEach(() => {
     //         cy.get('#username-input').type('initial-username')
